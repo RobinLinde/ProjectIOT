14da5102c6d63858ce4a9b8efebfba72a1eed6c5
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-02-17 14:22:42 +0100

    Add fadeLedTime

--- src/led.py
+++ src/led.py
@@ -16,4 +16,22 @@ def fadeLedX(x=1, pinNr=2, freq=20000, stepTime=0.001):
         count += 1
     pwm.deinit()
 
+def fadeLedTime(blinkTime=1, pinNr=2, freq=20000, stepTime=0.001):
+    count = 0
+    x = blinkTime / (stepTime * 2 * 1023)
+    dc = 1
+    while count < x:
+        while dc < 1023:
+            pwm = PWM(Pin(pinNr), freq=freq, duty=dc)
+            dc += 1
+            time.sleep(stepTime)
+        while dc > 1:
+            pwm = PWM(Pin(pinNr), freq=freq, duty=dc)
+            dc -= 1
+            time.sleep(stepTime)
+        count += 1
+    pwm.deinit()
+
 fadeLedX(2,12)
+
+fadeLedTime(5,12)
