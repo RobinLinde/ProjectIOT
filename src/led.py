81108e98ab138172a723b93ccd2c0302b71ad8a9
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-02-17 14:10:15 +0100

    Led Fade Function

--- src/led.py
+++ src/led.py
@@ -1,3 +1,19 @@
+import time
 from machine import Pin,PWM
 
-pwm = PWM(Pin(12), freq=20000, duty=512)
+def fadeLedX(x=1, pinNr=2, freq=20000, stepTime=0.001):
+    count=0
+    dc=1
+    while count < x:
+        while dc < 1023:
+            pwm = PWM(Pin(pinNr), freq=freq, duty=dc)
+            dc += 1
+            time.sleep(stepTime)
+        while dc > 1:
+            pwm = PWM(Pin(pinNr), freq=freq, duty=dc)
+            dc -= 1
+            time.sleep(stepTime)
+        count += 1
+    pwm.deinit()
+
+fadeLedX(2,12)
